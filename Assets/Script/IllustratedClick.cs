9de6262110447c8b22a213d0e3f7b26b7e431a7a
ZhaoWenZhuo <167313298+MineGuYan@users.noreply.github.com>
2024-10-04 22:43:50 +0800

    修复字体bug

--- Assets/Script/IllustratedClick.cs
+++ Assets/Script/IllustratedClick.cs
@@ -8,8 +8,7 @@ public class IllustratedClick : MonoBehaviour
 {
     public TMP_Text t;
     public Image i;
-    public Sprite R1;
-
+    public List<Sprite> R = new List<Sprite>();
     void Start()
     {
         
@@ -22,7 +21,7 @@ public class IllustratedClick : MonoBehaviour
 
     public void R1click()
     {
-        i.sprite = R1;
+        i.sprite = R[0];
         t.text = "神兽昵称：宁宁\r\n神兽信息：好色，猪批，幻想减肥\r\n技能1：睡觉\r\n技能2：瑟瑟\r\n技能3：红温";
     }
 }
