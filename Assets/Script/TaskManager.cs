828cfa2b8b2198dccb3debb2a14117d466b93554
ZhaoWenZhuo <167313298+MineGuYan@users.noreply.github.com>
2024-10-17 22:29:14 +0800

    任务进度保存功能

--- Assets/Script/TaskManager.cs
+++ Assets/Script/TaskManager.cs
@@ -2,26 +2,39 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using TMPro;
+using System.IO;
+using System;
 
 public class TaskManager : MonoBehaviour
 {
+    public static TaskManager instance;
+
     [Header("依赖对象")]
     [Tooltip("任务栏")] public GameObject task;
     [Tooltip("显示按钮")] public GameObject displayButton;
     [Tooltip("章节显示")] public TMP_Text TaskChap;
-    [Tooltip("任务描述")] public TMP_Text TaskText;
+    //[Tooltip("章节名称显示")] public TMP_Text TaskChapName;
+    [Tooltip("任务描述显示")] public TMP_Text TaskText;
     private bool isHide;
 
     [Header("任务列表")]
     public List<Tasks> tasks;
 
     [Header("任务进度")]
-    public int chap, order;
+    public TaskProgress taskProgress=new TaskProgress();
+
+    [System.Serializable]
+    public class TaskProgress
+    {
+        public int chap;
+        public int order;
+    }
 
     [System.Serializable]
     public class Tasks
     {
         [Tooltip("任务章节")] public string chapter;
+        //[Tooltip("章节名称")] public string chapName;
 
         [Header("任务列表")]
         public List<TaskList> tasksList;
@@ -29,13 +42,17 @@ public class TaskManager : MonoBehaviour
         [System.Serializable]
         public class TaskList
         {
-            [Tooltip("任务序号")] public string taskNumber;
+            [Tooltip("任务序号")] public string taskOrder;
             [Tooltip("任务描述")] public string description;
         }
     }
 
+    private void Awake()
+    {
+        instance = this;
+        LoadTaskProgress();
+    }
 
-    
     void Start()
     {
         isHide = false;
@@ -65,7 +82,42 @@ public class TaskManager : MonoBehaviour
 
     public void TaskDescriptionShow()
     {
-        TaskChap.text = tasks[chap].chapter;
-        TaskText.text = tasks[chap].tasksList[order].taskNumber+"\n"+ tasks[chap].tasksList[order].description;
+        TaskChap.text = tasks[taskProgress.chap].chapter;
+        //TaskChapName.text = tasks[taskProgress.chap].chapName;
+        TaskText.text = tasks[taskProgress.chap].tasksList[taskProgress.order].taskOrder+"\n"+ tasks[taskProgress.chap].tasksList[taskProgress.order].description;
+    }
+
+    public void SaveTaskProgress()
+    {
+        String json = JsonUtility.ToJson(taskProgress);
+        String filePath = Application.streamingAssetsPath + "/taskprogress.json";
+
+        using (StreamWriter sw = new StreamWriter(filePath))
+        {
+            sw.WriteLine(json);
+            sw.Close();
+            sw.Dispose();
+        }
+    }
+
+    public void LoadTaskProgress()
+    {
+        string json;
+        String filePath = Application.streamingAssetsPath + "/taskprogress.json";
+
+        if (File.Exists(filePath))
+        {
+            using (StreamReader sr = new StreamReader(filePath))
+            {
+                json = sr.ReadToEnd();
+                sr.Close();
+            }
+            taskProgress = JsonUtility.FromJson<TaskProgress>(json);
+        }
+        else
+        {
+            taskProgress.chap = 0;
+            taskProgress.order = 0;
+        }
     }
 }
