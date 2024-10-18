5c560d558919e7b612ee5ff8dd19c1a7d4faf974
ZhaoWenZhuo <167313298+MineGuYan@users.noreply.github.com>
2024-10-18 20:45:17 +0800

    对话触发逻辑重构

--- Assets/Script/DiaTrigger.cs
+++ Assets/Script/DiaTrigger.cs
@@ -8,17 +8,19 @@ public class DiaTrigger : MonoBehaviour
     public TextAsset textFile;
     public GameObject DialogBox;
     public TaskManager.TaskProgress Progress;
+    private Rigidbody2D RBody;
     void Start()
     {
-        
+        RBody = GetComponent<Rigidbody2D>();
     }
 
     void Update()
     {
-        
+        if (Input.GetKeyDown(KeyCode.F)) FindPlayer();     
     }
     public void TextTrigger()
     {
+        Debug.Log("Find");
         if (textFile != null && Progress == TaskManager.instance.taskProgress)
         {
             var tempLine = textFile.text.Split('\n');
@@ -29,5 +31,11 @@ public class DiaTrigger : MonoBehaviour
             Time.timeScale = 0.0f;
             DialogBox.SetActive(true);
         }
+    }
+
+    public void FindPlayer()
+    {
+        Collider2D collider = Physics2D.OverlapCircle(RBody.position, 1f, LayerMask.GetMask("Role"));
+        if (collider != null && collider.name == "Player") TextTrigger();
     }
 }
